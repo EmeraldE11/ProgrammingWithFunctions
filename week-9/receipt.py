3e9946dd7bffa8f12f7cf8831d68348cf9b70ff2
EmeraldE11 <mc20021@byui.edu>
2021-06-26 23:15:10 -0600

    week 10 prove assignment

--- week-9/receipt.py
+++ week-9/receipt.py
@@ -1,30 +1,53 @@
+# Import the datatime module so that
+# it can be used in this program.
+from datetime import datetime
+
 def main():
     products = read_products()
 
-    for i in products:
-        print(f"{i}: {products[i]}")
+    #for i in products:
+    #    print(f"{i}: {products[i]}")
 
+    print("Inkom Emporium\n")
     process_request(products)
+    print("\nThank you for shopping at the Inkom Emporium.")
+    date()
 
 def read_products():
 
     product_dict = {}
 
-    with open("products.csv", "rt") as products:
+    try:
+        with open("products.csv", "rt") as products:
 
-        next(products)
-        
-        for product in products:
-            product_info = product.strip().split(",")
-            product_dict[product_info[0]] = product_info[1], product_info[2]
-            
-    return product_dict
+            next(products)
+
+            for product in products:
+                product_info = product.strip().split(",")
+                product_dict[product_info[0]] = product_info[1], product_info[2]
+
+        return product_dict
+    except PermissionError as perm_err:
+        print()
+        print(f"You don't have permission to read products.csv.")
+        print("Run the program again and enter the name of a file that you are allowed to read.")
+    except FileNotFoundError as file_not_found_err:
+        print()
+        print(f"The file products.csv doesn't exist.")
+        print("Run the program again and enter the name of an existing file.")
+    except KeyError as key_err:
+        print()
+        print("The entered key does not exist in the products.csv.")
+        print("Please enter a valid key.")
+
 
 def process_request(dictionary):
     
     with open("request.csv", "rt") as requests:
 
         next(requests)
+        count = 0
+        total = 0
 
         for request in requests:
             request_info = request.strip().split(",")
@@ -35,6 +58,21 @@ def process_request(dictionary):
                 quantity = request_info[1]
                 print(f"{name}: {quantity} @ {price}")
 
+                count += int(quantity)
+                total += round(float(price) * float(quantity), 2)
+        
+        tax = total*.06
+        final = total + total * .06
+
+        print(f"\nNumber of Items: {count}")
+        print(f"Subtotal: {total}")
+        print(f"Sales Tax: {tax:.2f}")
+        print(f"Total: {final:.2f}")
+
+def date():
+    current_date_and_time = datetime.now()
+
+    print(f"{current_date_and_time:%c}")
 
 
 main()
