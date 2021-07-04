5ff67c661d256411312ad798aa679c4d91f47194
EmeraldE11 <mc20021@byui.edu>
2021-07-03 23:44:22 -0600

    project week 11- descriptions for functions, typo fix on proposal.txt

---
+++ project/bomb_defusal.py
@@ -0,0 +1,39 @@
+import math, random
+
+def main():
+    """configure bomb, present tutorial prompts, show where to find manual,
+    define bomb characteristics, set and run live timer(if possible at all)
+    (((create strike counter??? or one strike fail???)))"""
+
+    # set win and lose prompts and conditions
+
+
+def wires():
+    """randomly decide how many wires (3-5), set each a random color, create priority
+    list of which wire to cut position wise based on color count (likely organize
+    wire position by color to avoid too much complexity)"""
+
+    # set win and lose conditions
+
+
+def scrambler():
+    """DESCRIPTION: A list of passwords can provide the win condition, but the word will be
+    scrambled, and must be unscrambled in order for the win condition to be met.
+    Will be made by creating two corresponding lists, one of the scrambled word, the other
+    of the correct answer."""
+
+def color_text():
+    """POSSIBLE IDEA, UNSURE IF ABLE TO CREATE
+    a word will be displayed, the word will describe a color, but will also be displayed
+    as a different color. User must input the color the word is displayed in, not
+    the written prompt. multiple stages"""
+
+def binary():
+    """displays an a letter or number in binary (ASCII as conversion), and user must
+    input the correct corresponding character
+    (((unsure whether to just use numbers or include ASCII)))"""
+
+def speed_button():
+    """displays a character, user must replicate the display quickly. Displays
+    a starting prompt to avoid user being caught unaware. Multiple stages
+    (((Dependent on whether live timer is possible)))"""
